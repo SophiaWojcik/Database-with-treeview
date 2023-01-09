d733c7498c6e3356512e0c39f3be28297e6f72b6
973402 <120031271+SophiaWojcik@users.noreply.github.com>
2023-01-09 14:29:25 +0000

    finished database table,  worked on select buttons and resized tkinter table

--- main.py
+++ main.py
@@ -1,25 +1,106 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import sqlite3
+from tkinter import colorchooser
+from tabulate import tabulate
 
 
 root = Tk()
 root.title('The Weeknd- Album Database')
-root.geometry("500x500")
+root.geometry("1000x500")
 
-# Add Some Style
+# Insert data into database [year, album name, num of songs, sales, release year]
+data = [
+	["2011", "House of Balloons", 9, "100,000", 477],
+	["2011", "Thursday",9 , "95,000", 192],
+	["2011", "Echoes of Silence", 9, "95,000", 110],
+  ["2012", "Trilogy", 30, "1 015,000", 40],
+  ["2013", "Kiss Land", 12, "515,000", 86],
+  ["2015", "Beauty Behind the Madness", 14, "2 315,000", 37 ],
+  ["2016", "Starboy", 18, "1 440,000", 45],
+  ["2018", "My Dear Melancholy", 6, "270,000", 397], 
+  ["2020", "After Hours", 14, "1 005,000", 5],
+  ["2022", "Dawn FM", 24, "285,000", 15]
+  
+]
+
+
+
+
+#database
+#create a database or connect to one that exists 
+conn = sqlite3.connect ('Weeknd_Album.db')
+
+#Create cursor instance: able to execute sqlite statements
+cursor = conn.cursor ()
+
+#Create a table 
+cursor.execute("""CREATE TABLE if not exists albums ( 
+  release_year integer, 
+  album_name text,
+  numof_songs integer, 
+  sales integer, 
+  releaseYear_ranking integer)
+  """)
+
+#add fake data into table 
+for record in data: 
+  cursor.execute ("INSERT INTO albums VALUES (:release_year, :album_name, :numof_songs, :sales, :releaseYear_ranking)",
+#create a python dictionary:
+             {
+              'release_year': record[0],
+              'album_name': record[1],
+              'numof_songs': record[2],
+              'sales': record[3],
+              'releaseYear_ranking': record[4]
+             }
+            ) 
+
+
+  
+#Commit changes 
+conn.commit()
+
+#Close connection to database
+conn.close ()
+
+
+def query_database ():
+#database
+#create a database or connect to one that exists 
+  conn = sqlite3.connect ('Weeknd_Album.db')
+
+#Create cursor instance: able to execute sqlite statements
+  cursor = conn.cursor ()
+
+  #print database table
+  cursor.execute ("SELECT * FROM albums")
+  records = cursor.fetchall()
+  
+  print(tabulate(records, tablefmt = 'fancy_grid',headers= ["Album Release Year", "Album Name", "Number of Songs", "Sales", "Release Year Ranking"]))
+  
+  #Commit changes 
+  conn.commit()
+
+#Close connection to database
+  conn.close ()
+
+
+# Style format
 style = ttk.Style()
 
-# Pick A Theme
+# Table theme
 style.theme_use('default')
 
 # Configure the Treeview Colors
 style.configure("Treeview",
-	background="#D3D3D3",
+	background="silver",
 	foreground="black",
 	rowheight=25,
 	fieldbackground="#D3D3D3")
 
-# Change Selected Color
+# Change Selected Color Treeview
 
 style.map('Treeview',
 #Python list with tuple inside. 
@@ -40,21 +121,19 @@ my_tree.pack()
 # Configure the Scrollbar
 tree_scroll.config(command=my_tree.yview)
 
-# Define Our Columns
+# Define database Columns
 my_tree['columns'] = ("Album Release Year", "Album Name", "Number of Songs", "Sales", "Release year Ranking")
 
-# Format Our Columns
+# Format Columns
 my_tree.column("#0", width=0, stretch=NO)
 my_tree.column("Album Release Year", anchor=W, width=150)
-my_tree.column("Album Name", anchor=W, width=140)
+my_tree.column("Album Name", anchor=W, width=180)
 my_tree.column("Number of Songs", anchor=CENTER, width=140)
 my_tree.column("Sales", anchor=CENTER, width=140)
 my_tree.column("Release year Ranking", anchor=CENTER, width=170)
 
 
-
-
-# Create Headings
+# Create database headings
 my_tree.heading("#0", text="", anchor=W)
 my_tree.heading("Album Release Year", text="Album Release Year", anchor=W)
 my_tree.heading("Album Name", text="Album Name", anchor=W)
@@ -64,13 +143,6 @@ my_tree.heading("Release year Ranking", text="Release year Ranking", anchor=CENT
 
 
 
-# Add Fake Data
-data = [
-	["John", "Elder", 1, "123 Elder St.", "Las Vegas"],
-	["Mary", "Smith", 2, "435 West Lookout", "Chicago"],
-	["Tim", "Tanaka", 3, "246 Main St.", "New York"]
-	
-]
 
 # Create Striped Row Tags
 my_tree.tag_configure('oddrow', background="white")
@@ -80,7 +152,7 @@ my_tree.tag_configure('evenrow', background="lightgreen")
 global count
 count = 0
 
-
+#Number of records inserted into database (even and odd)
 for record in data:
 	if count % 2 == 0:
 		my_tree.insert(parent='', index='end', iid=count, text='', values=(record[0], record[1], record[2], record[3], record[4]), tags=('evenrow',))
@@ -90,40 +162,58 @@ for record in data:
 	count += 1
 
 
-# Add Record Entry Boxes
+# Add Display Record Entry Boxes (after selecting a record)
 data_frame = LabelFrame(root, text="Record")
 data_frame.pack(fill="x", expand="yes", padx=20)
 
-fn_label = Label(data_frame, text="Album Release Year")
-fn_label.grid(row=0, column=0, padx=10, pady=10)
-fn_entry = Entry(data_frame)
-fn_entry.grid(row=0, column=1, padx=10, pady=10)
+ary_label = Label(data_frame, text="Album Release Year")
+ary_label.grid(row=0, column=0, padx=10, pady=10)
+ary_entry = Entry(data_frame)
+ary_entry.grid(row=0, column=1, padx=10, pady=10)
 
-ln_label = Label(data_frame, text="Album Name")
-ln_label.grid(row=0, column=2, padx=10, pady=10)
-ln_entry = Entry(data_frame)
-ln_entry.grid(row=0, column=3, padx=10, pady=10)
+an_label = Label(data_frame, text="Album Name")
+an_label.grid(row=0, column=2, padx=10, pady=10)
+an_entry = Entry(data_frame)
+an_entry.grid(row=0, column=3, padx=10, pady=10)
 
-id_label = Label(data_frame, text="Number of Songs")
-id_label.grid(row=0, column=4, padx=10, pady=10)
-id_entry = Entry(data_frame)
-id_entry.grid(row=0, column=5, padx=10, pady=10)
+ns_label = Label(data_frame, text="Number of Songs")
+ns_label.grid(row=0, column=4, padx=10, pady=10)
+ns_entry = Entry(data_frame)
+ns_entry.grid(row=0, column=5, padx=10, pady=10)
 
-address_label = Label(data_frame, text="Sales")
-address_label.grid(row=1, column=0, padx=10, pady=10)
-address_entry = Entry(data_frame)
-address_entry.grid(row=1, column=1, padx=10, pady=10)
+sales_label = Label(data_frame, text="Sales")
+sales_label.grid(row=1, column=0, padx=10, pady=10)
+sales_entry = Entry(data_frame)
+sales_entry.grid(row=1, column=1, padx=10, pady=10)
 
-city_label = Label(data_frame, text="Release year Ranking")
-city_label.grid(row=1, column=2, padx=10, pady=10)
-city_entry = Entry(data_frame)
-city_entry.grid(row=1, column=3, padx=10, pady=10)
+ryr_label = Label(data_frame, text="Release year Ranking")
+ryr_label.grid(row=1, column=2, padx=10, pady=10)
+ryr_entry = Entry(data_frame)
+ryr_entry.grid(row=1, column=3, padx=10, pady=10)
 
-state_label = Label(data_frame, text="Album Release Year")
-state_label.grid(row=1, column=4, padx=10, pady=10)
-state_entry = Entry(data_frame)
-state_entry.grid(row=1, column=5, padx=10, pady=10)
+#Display Selected Record 
+def select_record ():
+  #Clear entry boxes
+  ary_entry.delete (0, END)
+  an_entry.delete (0,End)
+  ns_entry.delete (0, End)
+  sales_entry.delete (0,END)
+  ryr_entry.delete (0,END)
 
+  #Grab record number to search through records
+  selected = my_tree.focus ()
+
+  #Grab record values
+  values = my_tree.item (selected, 'values')
+
+  #output entry boxes
+  ary_entry.insert (0, values [0])
+  an_entry.insert (0,values [1])
+  ns_entry.insert (0, values [2])
+  sales_entry.insert (0,values [3])
+  ryr_entry.insert (0,values [4])
+  
+  
 
 
 
@@ -152,11 +242,11 @@ move_up_button.grid(row=0, column=5, padx=10, pady=10)
 move_down_button = Button(button_frame, text="Move Down")
 move_down_button.grid(row=0, column=6, padx=10, pady=10)
 
-select_record_button = Button(button_frame, text="Select Record")
+select_record_button = Button(button_frame, text="Select Record", command =select_record)
 select_record_button.grid(row=0, column=7, padx=10, pady=10)
 
 
 
-
+query_database ()
 
 root.mainloop()
